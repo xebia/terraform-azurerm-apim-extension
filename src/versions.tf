e6880f7e427c68722aac29c783c1615d96610a8b
Marcin Słowikowski <59068696+msl0@users.noreply.github.com>
2025-12-05 14:05:54 +0100

    Updated provider's version constraint

--- src/versions.tf
+++ src/versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.14.0"
+      version = "~> 4.0"
     }
     azapi = {
       source  = "azure/azapi"
-      version = "1.14.0"
+      version = "~> 1.0"
     }
   }
 }
